b3702729e45d7f3fd8407838c169af70b57a3895
Antonio García-Domínguez <a.garcia-dominguez@york.ac.uk>
2022-07-13 10:25:19 +0100

    Create epsilon-mdplugin.jar from script as well - no need to export from Eclipse

--- magicdraw-plugin/org.eclipse.epsilon.emc.magicdraw.modelapi/create-magicdraw-plugin.sh
+++ magicdraw-plugin/org.eclipse.epsilon.emc.magicdraw.modelapi/create-magicdraw-plugin.sh
@@ -7,13 +7,20 @@ fi
 
 PLUGIN_DIR="$(readlink -f "$1/org.eclipse.epsilon.emc.magicdraw.mdplugin.remote")"
 
-if ! test -d "$PLUGIN_DIR"; then
-  mkdir "$PLUGIN_DIR"
-  ln -s "$(readlink -f ../org.eclipse.epsilon.emc.magicdraw.mdplugin.remote/resources/magicdraw-plugin.xml)" "$PLUGIN_DIR/plugin.xml"
+# Always recreate the plugin folder from scratch
+if test -d "$PLUGIN_DIR"; then
+  rm -r "$PLUGIN_DIR"
 fi
+mkdir "$PLUGIN_DIR"
+ln -s "$(readlink -f ../org.eclipse.epsilon.emc.magicdraw.mdplugin.remote/resources/magicdraw-plugin.xml)" "$PLUGIN_DIR/plugin.xml"
 
+# Add the remote model access API and its dependencies
 cd "$(dirname "$0")"
 mvn clean package
 mvn dependency:copy-dependencies "-DoutputDirectory=$PLUGIN_DIR"
 ls target/org.eclipse.epsilon.emc.magicdraw*.jar | grep -v uberjar | xargs cp -t "$PLUGIN_DIR"
 ls target/org.eclipse.epsilon.emc.magicdraw*.jar | grep uberjar | xargs cp -t ../../bundles/org.eclipse.epsilon.emc.magicdraw.remote/lib/
+
+# Add the remote model access server
+cd ../org.eclipse.epsilon.emc.magicdraw.mdplugin.remote
+jar cf "$PLUGIN_DIR/epsilon-mdplugin.jar" -C bin/ .
