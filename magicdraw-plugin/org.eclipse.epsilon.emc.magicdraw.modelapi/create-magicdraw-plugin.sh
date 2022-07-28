1b528fa9755bdd709beb497ff6fde8d64788e5d1
Antonio García-Domínguez <a.garcia-dominguez@york.ac.uk>
2022-07-28 15:43:17 +0100

    Generate a plugin ZIP with version, SHA1 and timestamp

--- magicdraw-plugin/org.eclipse.epsilon.emc.magicdraw.modelapi/create-magicdraw-plugin.sh
+++ magicdraw-plugin/org.eclipse.epsilon.emc.magicdraw.modelapi/create-magicdraw-plugin.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ "$#" != 1 ]; then
   echo "Usage: $0 path/to/magicdraw/plugins"
   exit 1
@@ -17,6 +19,8 @@ ln -s "$(readlink -f ../org.eclipse.epsilon.emc.magicdraw.mdplugin.remote/resour
 
 # Add the remote model access API and its dependencies
 cd "$SCRIPT_DIR"
+BUNDLE_VERSION="$(grep Bundle-Version ../../bundles/org.eclipse.epsilon.emc.magicdraw.remote/META-INF/MANIFEST.MF  | awk '{print $2}' | cut --delim=. -f1-3)"
+SHORT_REV="$(git rev-parse --short HEAD)"
 mvn clean package
 mvn dependency:copy-dependencies "-DoutputDirectory=$PLUGIN_DIR"
 ls target/org.eclipse.epsilon.emc.magicdraw*.jar | grep -v uberjar | xargs cp -t "$PLUGIN_DIR"
@@ -28,4 +32,4 @@ jar cf "$PLUGIN_DIR/epsilon-mdplugin.jar" -C bin/ .
 
 # Create a redistributable ZIP with the MagicDraw plugin
 cd "$PLUGIN_DIR/.."
-zip -r "$SCRIPT_DIR/epsilon-mdplugin.zip" "$(basename "$PLUGIN_DIR")"
+zip -r "$SCRIPT_DIR/cameo-mdplugin-$BUNDLE_VERSION-$SHORT_REV-$(date +'%Y%m%d%H%M%S').zip" "$(basename "$PLUGIN_DIR")"
